78baa24e36920593ba5f788c0de1d7a4eb5cbc4f
Mark Chimes <mark-chimes@users.noreply.github.com>
2022-06-11 14:08:14 +0200

    Separate signal and conversation bars.

--- GameController.gd
+++ GameController.gd
@@ -6,19 +6,21 @@ extends Node2D
 # var b = "text"
 
 export var signal_half_width = 275
-export var signal_fill_speed = 20
-export var signal_reduction_speed = 5
+export var signal_fill_speed = 50
+export var signal_reduction_speed = 20
+export var signal_threshold = 80
+export var convo_fill_speed = 8
+export var convo_reduction_speed = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 func _process(delta):
 	var player_x = $PlayerController/Player.position.x
 	var signal_x = $SignalPoint.position.x
 	var signal_bar = $CanvasLayer/SignalBar/ProgressBar
-
+	var convo_bar = $CanvasLayer/ConvoBar/ProgressBar
 	
 	if abs(signal_x - player_x) <= signal_half_width: 
 		if signal_bar.value <= 100: 
@@ -26,7 +28,19 @@ func _process(delta):
 	else: 
 		if signal_bar.value >= 0: 
 			signal_bar.value = signal_bar.value - signal_reduction_speed*delta
-
+	
+	var styleBox = signal_bar.get("custom_styles/fg") 
+	if signal_bar.value >= signal_threshold: 
+		styleBox.bg_color = Color(0, 1, 0)
+		if convo_bar.value <= 100: 
+			convo_bar.value += convo_fill_speed*delta
+	else: 
+		styleBox.bg_color = Color(1, 0, 0)
+		convo_bar.value -= convo_reduction_speed*delta
+		
+	
+	
+	
 	var back = $background/WildernessBackground
 	if Input.is_action_just_pressed("train_up"): 
 		back.train_speed *= sqrt(2)
