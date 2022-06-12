ddccfb464a17076a94acd86a877b23f7a8c8c8c3
Mark Chimes <mark-chimes@users.noreply.github.com>
2022-06-13 00:47:54 +0200

    Passengers trip

--- Passenger.gd
+++ Passenger.gd
@@ -29,14 +29,15 @@ export var max_time_for_state_change = 6
 var time_since_last_state_change = 0
 var time_til_next_state_change = 0
 
-export var shove_strength = 2.5
+export var shove_strength = 2
 var velocity = 0
 const FRICTION = 200
 const INVINCIBILITY_TIME = 1
 var invincibility_timer = 0
 var is_invincible
 
-const MAX_VELOCITY = 1000
+const MAX_VELOCITY = 1200
+const TRIP_VELOCITY = 800
 const COMFORT_VELOCITY = 500
 
 var is_npc = true
@@ -60,17 +61,22 @@ func _process(delta):
 	if passenger_state == PassengerState.SHOVE and abs(velocity) < COMFORT_VELOCITY:
 		passenger_state = PassengerState.WALK
 		$AnimatedSprite.play("walk")
+	if passenger_state in [PassengerState.WALK, PassengerState.SHOVE] and abs(velocity) > TRIP_VELOCITY: 
+		passenger_state = PassengerState.TRIP
+		$AnimatedSprite.play("trip")
 	elif passenger_state == PassengerState.WALK and abs(velocity) > COMFORT_VELOCITY: 
 		passenger_state = PassengerState.SHOVE
 		$AnimatedSprite.play("shove")
-	
+
 	if passenger_state == PassengerState.SHOVE:
 		if velocity > 0: 
 			$AnimatedSprite.flip_h = false
 		else: 
-			$AnimatedSprite.flip_h = true	
-	
+			$AnimatedSprite.flip_h = true
 		
+		# randomly trip
+
+			
 	if passenger_state == PassengerState.WALK: 
 		$AnimatedSprite.play("walk")
 		if position.x < target_x - seating_half_width:
@@ -97,7 +103,7 @@ func _process(delta):
 				$AnimatedSprite.play("idle")
 				reset_wait()
 				
-	elif passenger_state in [PassengerState.SIT, PassengerState.HANDHOLD] : 
+	elif passenger_state in [PassengerState.SIT, PassengerState.HANDHOLD, PassengerState.TRIP] : 
 		if time_since_last_state_change >= time_til_next_state_change: 
 			emit_signal("stop_using_target", self)
 		else: 
@@ -113,10 +119,16 @@ func velocity_calculations(delta):
 		return
 	velocity = clamp(velocity, -MAX_VELOCITY, MAX_VELOCITY)
 	
-	if velocity > target_velocity:
-		velocity -= FRICTION * delta
-	else:
-		velocity += FRICTION * delta
+	if passenger_state == PassengerState.TRIP: 
+		if velocity > 0:
+			velocity -= FRICTION * delta
+		else:
+			velocity += FRICTION * delta
+	else: 
+		if velocity > target_velocity:
+			velocity -= FRICTION * delta
+		else:
+			velocity += FRICTION * delta
 		
 	invincibility_timer -= delta
 	if invincibility_timer > 0:
@@ -125,7 +137,7 @@ func velocity_calculations(delta):
 	else:
 		$Sprite2.visible = false
 		is_invincible = false
-	if position.x < 0 or position.x > 5000:
+	if position.x < 200 or position.x > 5000:
 		velocity = -velocity
 		
 	position.x += velocity * delta
